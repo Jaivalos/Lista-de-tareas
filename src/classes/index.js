54f2c1dcdfbcd4c6cde96c59a0aa74f6dda9aeee
Javier Avalos <jaivalos18j2@gmail.com>
2022-03-26 16:26:53 -0600

    Pruebas de mostrado en consolo de los Todos

---
+++ src/classes/index.js
@@ -0,0 +1,10 @@
+
+//Creamos este archivo para realizar la importacion de todas las clases que utilizaremos en el proyecto, exportamos las variables que necesitamos y luego lo importamos en el index
+
+import { Todo } from './todo.class.js';
+import { TodoList } from './todo-list.class.js';
+
+export{
+    Todo,
+    TodoList,
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@
 import './styles.css';
 
 //Importacion de clases
-import { Todo } from './Clases/todo.class';
+import { Todo, TodoList } from './classes';
 
-//Crear un elemento de tipo Todo y mostrarlo en consola
-const tarea = new Todo( 'Estudiar Css' );
-console.log( tarea );
+//Creacion de elemento TodoList para guardar los todos
+const todoList = new TodoList();
+
+//Creamos elementos Todo
+const tarea1 = new Todo( 'Estudiar Css' );
+const tarea2 = new Todo( 'Estudiar JS' );
+const tarea3 = new Todo( 'Estudiar HTML' );
+
+//Guardamos y mostramos
+todoList.nuevoTodo(tarea1);
+todoList.nuevoTodo(tarea2);
+todoList.nuevoTodo(tarea3);
+
+console.log(todoList);
